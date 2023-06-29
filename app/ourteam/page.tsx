6e0e217c8dbcdfd9e39fe075b12bdb6d8d97b7f5
realkelvinworld <4231210031@live.gctu.edu.gh>
2023-06-29 15:21:58 -0700

    kk

--- app/ourteam/page.tsx
+++ app/ourteam/page.tsx
@@ -28,7 +28,7 @@ function page() {
 
           <div>
             <div className="flex flex-row justify-around flex-wrap">
-              <div>
+              <div className="w-96">
                 <Image
                   src={PentImage3}
                   alt="Mr.Yeboah Faculty Head"
@@ -39,71 +39,77 @@ function page() {
                   Mr.Isaac Kumah Yeboah <br />
                   <span className="text-lg font-medium">(Head of IDC)</span>
                 </h2>
+                <p className="pb-4 text-lg text-center font-medium">
+                  {" "}
+                  Isaac Kuma Yeboah: Awarded "Most Promising Researcher of the
+                  Year 2012" by Regent University College of Science and
+                  Technology. Received the "Zonal Championship Medal" by Ghana
+                  Robotic Academy Foundation in October 2012. Project leader for
+                  the Texas Instrument innovation challenge Europe analog design
+                  contest 2014.
+                </p>
               </div>
-              <ul className=" font-medium lg:text-xl w-[50rem]">
-                Isaac Kuma Yeboah: Awarded "Most Promising Researcher of the
-                Year 2012" by Regent University College of Science and
-                Technology. Received the "Zonal Championship Medal" by Ghana
-                Robotic Academy Foundation in October 2012. Project leader for
-                the Texas Instrument innovation challenge Europe analog design
-                contest 2014. <br />
-                <br />
-                <span className=" font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Expertise
-                </span>
-                <li>
-                  Isaac Kuma Yeboah: Head and lead researcher at the
-                  International Digital Centre of Pentecost University.
-                  Specialized in energy physics with a BSc. in physics from the
-                  School of Physical Science, University of Cape Coast, Ghana.
-                  Holds an MSc. in electronic science and technology, majoring
-                  in circuits and systems from the School of Information
-                  Engineering, Wuhan University of Technology, China. Research
-                  interests include the application of artificial intelligence
-                  at the edge, digital twin technology in industry automation,
-                  digital communication, and wireless technology.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Publications
-                </span>
-                <li>
-                  Published eight engineering and scientific research papers in
-                  both local and international journals. Authored the book
-                  "Optimization of Ultrasonic Flow Meter for Crude Oil Metering"
-                  published by LAMBERT Academic Publishing in Germany.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Professional Memberships
-                </span>
-                <li>
-                  Member of the International Engineering and Technology
-                  Institute, World Scientific and Engineering Academy Society,
-                  and Ghana Science Association.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Experience
-                </span>
-                <li>
-                  Worked as a project coordinator at the Industrial Research
-                  Institute of the Council for Scientific and Industrial
-                  Research Ghana. Served as a lecturer at All Nations University
-                  College and Regent University College of Science and
-                  Technology.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Committee Involvement
-                </span>
-                <li>
-                  Served as a scientific committee member reviewing papers for
-                  the 10th IASME/WSEAS International Conference on Heat
-                  Transfer, Thermal Engineering, and Environment held in
-                  Istanbul, Turkey.
-                </li>
-              </ul>
+              <div>
+                <ul className=" font-medium lg:text-xl w-auto lg:w-[50rem]">
+                  <span className=" font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Expertise
+                  </span>
+                  <li>
+                    Isaac Kuma Yeboah: Head and lead researcher at the
+                    International Digital Centre of Pentecost University.
+                    Specialized in energy physics with a BSc. in physics from
+                    the School of Physical Science, University of Cape Coast,
+                    Ghana. Holds an MSc. in electronic science and technology,
+                    majoring in circuits and systems from the School of
+                    Information Engineering, Wuhan University of Technology,
+                    China. Research interests include the application of
+                    artificial intelligence at the edge, digital twin technology
+                    in industry automation, digital communication, and wireless
+                    technology.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Publications
+                  </span>
+                  <li>
+                    Published eight engineering and scientific research papers
+                    in both local and international journals. Authored the book
+                    "Optimization of Ultrasonic Flow Meter for Crude Oil
+                    Metering" published by LAMBERT Academic Publishing in
+                    Germany.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Professional Memberships
+                  </span>
+                  <li>
+                    Member of the International Engineering and Technology
+                    Institute, World Scientific and Engineering Academy Society,
+                    and Ghana Science Association.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Experience
+                  </span>
+                  <li>
+                    Worked as a project coordinator at the Industrial Research
+                    Institute of the Council for Scientific and Industrial
+                    Research Ghana. Served as a lecturer at All Nations
+                    University College and Regent University College of Science
+                    and Technology.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Committee Involvement
+                  </span>
+                  <li>
+                    Served as a scientific committee member reviewing papers for
+                    the 10th IASME/WSEAS International Conference on Heat
+                    Transfer, Thermal Engineering, and Environment held in
+                    Istanbul, Turkey.
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -118,52 +124,48 @@ function page() {
 
           <div>
             <div className="flex flex-row justify-around flex-wrap-reverse">
-              <ul className=" font-medium lg:text-xl  w-[50rem]">
-                Matthew Asiedu: Co-author of the research publication on
-                "Fabrication and Optimization of Bi-Quad Antenna and
-                Energy-Efficient Balanced RF Power Amplifier for 5G-LTE
-                Multi-Carrier Applications" featured in Volume 3 (2002), Issue
-                3, Pages 874-885 by the International Telecommunication Union in
-                2022. <br />
-                <br />
-                <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Expertise
-                </span>
-                <li>
-                  Assistant to the International Digital Centre at Pentecost
-                  University. Holds a BSc. in Information Technology from
-                  Pentecost University. Specialized in the area of Credit Card
-                  Fraud Detection Using Transactional Alert Techniques during
-                  his first-degree thesis. Research interests include computer
-                  forensics, information security, artificial intelligence, and
-                  expert systems.
-                </li>
-                <br />
-                <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Work Experience
-                </span>
-                <li>
-                  I.T Support Technician from 2012 to 2016. Management
-                  Information System Officer (MIS Officer) from 2016 to 2021
-                </li>
-                <br />
-                <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Student Mentorship
-                </span>
-                <li>
-                  Student mentor in the area of Information Technology (I.T) at
-                  Pentecost University from 2014 to the present.
-                </li>
-                <br />
-                <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Areas of Interest
-                </span>
-                <li>
-                  Research interests include computer forensics, information
-                  security, artificial intelligence, and expert systems.
-                </li>
-              </ul>
               <div>
+                <ul className=" font-medium lg:text-xl w-auto lg:w-[50rem]">
+                  <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Expertise
+                  </span>
+                  <li>
+                    Assistant to the International Digital Centre at Pentecost
+                    University. Holds a BSc. in Information Technology from
+                    Pentecost University. Specialized in the area of Credit Card
+                    Fraud Detection Using Transactional Alert Techniques during
+                    his first-degree thesis. Research interests include computer
+                    forensics, information security, artificial intelligence,
+                    and expert systems.
+                  </li>
+                  <br />
+                  <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Work Experience
+                  </span>
+                  <li>
+                    I.T Support Technician from 2012 to 2016. Management
+                    Information System Officer (MIS Officer) from 2016 to 2021
+                  </li>
+                  <br />
+                  <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Student Mentorship
+                  </span>
+                  <li>
+                    Student mentor in the area of Information Technology (I.T)
+                    at Pentecost University from 2014 to the present.
+                  </li>
+                  <br />
+                  <span className="font-semibold  px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Areas of Interest
+                  </span>
+                  <li>
+                    Research interests include computer forensics, information
+                    security, artificial intelligence, and expert systems.
+                  </li>
+                </ul>
+              </div>
+
+              <div className="w-96">
                 <Image
                   src={PentImage3}
                   alt="Mr.Yeboah Faculty Head"
@@ -176,6 +178,15 @@ function page() {
                     (Executive Assistant at IDC)
                   </span>
                 </h2>
+                <p className="pb-4 text-lg font-medium text-center">
+                  {" "}
+                  Matthew Asiedu: Co-author of the research publication on
+                  "Fabrication and Optimization of Bi-Quad Antenna and
+                  Energy-Efficient Balanced RF Power Amplifier for 5G-LTE
+                  Multi-Carrier Applications" featured in Volume 3 (2002), Issue
+                  3, Pages 874-885 by the International Telecommunication Union
+                  in 2022.
+                </p>
               </div>
             </div>
           </div>
@@ -191,7 +202,7 @@ function page() {
 
           <div>
             <div className="flex flex-row justify-around flex-wrap">
-              <div>
+              <div className="w-96">
                 <Image
                   src={PentImage3}
                   alt="Mr.Yeboah Faculty Head"
@@ -204,51 +215,55 @@ function page() {
                     (Research Fellow at IDC)
                   </span>
                 </h2>
+                <p className="pb-4 text-lg font-medium text-center">
+                  {" "}
+                  Co-author of publications on advanced communication
+                  technologies and green human resource management. Co-author of
+                  practical books on cost engineering and engineering practices.
+                </p>
               </div>
-              <ul className=" font-medium lg:text-xl w-[50rem]">
-                Co-author of publications on advanced communication technologies
-                and green human resource management. Co-author of practical
-                books on cost engineering and engineering practices. <br />
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Expertise
-                </span>
-                <li>
-                  Research interests in cost engineering/quantity surveying,
-                  sustainable engineering, AI, and computing. Application of
-                  artificial intelligence techniques and computational methods
-                  in engineering, including machine learning, data analytics,
-                  optimization algorithms, and computer-aided design.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Professionalism
-                </span>
-                <li>
-                  Solid educational background with a Bachelor of Science degree
-                  in Quantity Surveying and Construction Economics. Additional
-                  certifications in AI technologies, computer-aided design,
-                  programming, and animation.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Collaboration
-                </span>
-                <li>
-                  Multidisciplinary approach and collaborative efforts to drive
-                  innovation in cost engineering, sustainable engineering, and
-                  AI and computing. Valuable asset to the academic and research
-                  community.
-                </li>
-                <br />
-                <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
-                  Team Members in Science Department
-                </span>
-                <li>
-                  Researcher at the International Digital Centre affiliated with
-                  Pentecost University.
-                </li>
-              </ul>
+              <div>
+                <ul className=" font-medium lg:text-xl w-auto lg:w-[50rem]">
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Expertise
+                  </span>
+                  <li>
+                    Research interests in cost engineering/quantity surveying,
+                    sustainable engineering, AI, and computing. Application of
+                    artificial intelligence techniques and computational methods
+                    in engineering, including machine learning, data analytics,
+                    optimization algorithms, and computer-aided design.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Professionalism
+                  </span>
+                  <li>
+                    Solid educational background with a Bachelor of Science
+                    degree in Quantity Surveying and Construction Economics.
+                    Additional certifications in AI technologies, computer-aided
+                    design, programming, and animation.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Collaboration
+                  </span>
+                  <li>
+                    Multidisciplinary approach and collaborative efforts to
+                    drive innovation in cost engineering, sustainable
+                    engineering, and AI and computing. Valuable asset to the
+                    academic and research community.
+                  </li>
+                  <br />
+                  <span className="font-semibold px-2 bg-yellow-500 border-l-4 border-blue-800">
+                    Team Members in Science Department
+                  </span>
+                  <li>
+                    Researcher at the International Digital Centre affiliated
+                    with Pentecost University.
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
