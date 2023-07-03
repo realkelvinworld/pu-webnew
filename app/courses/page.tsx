7a70b2d4d5fddc1dd119a6f9536eb6000cdb7f70
realkelvinworld <4231210031@live.gctu.edu.gh>
2023-07-02 20:22:06 -0700

    kk

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { course1, courseOne } from "../components/db";
 
 function page() {
   return (
-    <div className="pb-72 relative">
+    <div className="pb-72 relative flex justify-center">
       <div className="gridbg inset-0 absolute z-10" />
-      <section className="relative z-10 px-10">
-        <h1 className="text-center">
-          Courses Available Available at the Department
-        </h1>
+      <section className=" relative z-10 px-10 lg:w-2/3 w-auto">
+        <div className="pt-20">
+          <h1 className="text-center text-4xl font-semibold">
+            Courses Available Available at the Department
+          </h1>
+        </div>
+        <div className=" py-6">
+          <p className=" text-center font-medium text-gray-400">
+            The Department of Systems Engineering offers essential courses in
+            systems analysis, integration, risk management, project management,
+            and simulation modeling. These courses equip students with in-demand
+            skills for industries like aerospace, defense, transportation, and
+            healthcare. Gain a strong foundation in systems thinking,
+            problem-solving, and project management to excel in systems
+            engineering and related fields.
+          </p>
+        </div>
+        {/* Course content */}
+        <div className="border text-center">
+          {courseOne.map((c1) => {
+            const { id, title, description } = c1;
+            return (
+              <div key={id} className="py-20">
+                <h1 className="text-2xl font-semibold">{title}</h1>
+                <p className="font-medium">{description}</p>
+              </div>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
